940396fb2415b8499c5f0843c2b80feb7791e0bf
joanavrsilva <joanavrsilva@gmail.com>
2021-09-15 18:40:33 +0000

    Start game and score code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,33 @@
-console.log ("connected!")
+// Play and score functions
+
+// Images
+const handOptions = {
+    'rock': "assets/images/hand-rock.png",
+    'paper': "assets/images/hand-paper.png",
+    'scissors': "assets/images/hand-scissors.png",
+    'spock': "assets/images/hand-spock.png",
+    'lizard': "assets/images/lizard.png",
+}
+
+// Winning matches
+
+const handwins = {
+    'rock': ['scissors', 'lizard'],
+    'paper': ['rock', 'spock'],
+    'scissors': ['paper', 'lizard'],
+    'spock': ['rock', 'scissors'],
+    'lizard': ['paper', 'spock']
+};
+
+// Score variables
+let score = 0;
+let scoreComputer = 0;
+
+// Get hands options
+var hands = document.getElementById("hands");
+
+//get result section elements
+var results = document.getElementById("results");
+var userChoice = document.getElementById("userChoiceImage");
+var computerChoice = document.getElementById("computerChoiceImage");
+
